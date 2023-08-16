1299fa2a1be5cbe57573b152f3bd8fadb96e783b
Henry Pinchao <bengy_p@hotmail.com>
2023-08-16 17:21:09 -0500

    Ordendar las llaves de un diccionario por el valor De un listado de tuplas devolver las tuplas que tengan el mayor valor. Crear un mensajE que diga: Los caracteres que más se repiten con x repeticiones son: - C = 9

--- ejercicios.py
+++ ejercicios.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 # 1. Eliminar los espacios en blanco de un string
 # y devolver una lista con los caracteres restantes.
-string = 'Hola Mundo Henry este es mi string'
+string = 'Hola Mundo Henry este es mi string cc'
 
 
 def quita_espacios(texto):
@@ -44,7 +44,48 @@ pprint(contados, width=1)
 
 # Ordendar las llaves de un diccionario por el valor que tienen y devolver una lista que contiene tuplas
 # [("a", 3),("b",2),("c",4),("d",4)]
-dic_1 = {"a": 3, "b": 2, "c": 9, "d": 7, "e": 6}
+dic_1 = {"a": 3, "b": 2, "c": 10, "d": 10, "e": 6}
 dic_ordenado = dict(sorted(dic_1.items(), key=lambda x: x[1]))
 print(dic_1)
 print(dic_ordenado)
+
+# Modo 2
+
+
+def ordena(diction):
+    return sorted(diction.items(),
+                  key=lambda key: key[1],
+                  reverse=True,
+                  )
+
+
+ordenados = ordena(dic_1)
+pprint(ordenados, width=1)
+
+
+# De un listado de tuplas devolver las tuplas que tengan el mayor valor.
+def mayores_tuplas(lista):
+    maximo = lista[0][1]
+    respuesta = {}
+    for orden in lista:
+        if maximo > orden[1]:
+            break
+        respuesta[orden[0]] = orden[1]
+    return respuesta
+
+
+tupla_mayor = mayores_tuplas(ordenados)
+pprint(tupla_mayor)
+
+# Crear un mensaej que diga: Los caracteres que más se repiten con x repeticiones son: -C = 9
+
+
+def crea_mensaje(dicciona):
+    mensaje = "Los que más se repiten son: \n"
+    for key, valor in dicciona.items():
+        mensaje += f"- {key} con {valor} repeditos"
+    return mensaje
+
+
+mensaje = crea_mensaje(tupla_mayor)
+print(mensaje)
